f1075151fcb64aa1e4d6dd6fdb954795b79c9318
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-22 15:50:58 -0300

    v3.1: Google Sign-In restrito a emails cadastrados

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 22,
-    versionName: "3.0",
+    versionCode: 23,
+    versionName: "3.1",
     url: "https://locarpay-api.vercel.app/download/locarpay.apk"
   });
 }
