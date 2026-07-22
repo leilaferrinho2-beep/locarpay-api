5a2c087750bc76d1281d6ae479169d2917c5b5a2
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-22 18:56:37 -0300

    v3.2: fix verificacao de versao - URL correta do endpoint

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 23,
-    versionName: "3.1",
+    versionCode: 24,
+    versionName: "3.2",
     url: "https://locarpay-api.vercel.app/download/locarpay.apk"
   });
 }
