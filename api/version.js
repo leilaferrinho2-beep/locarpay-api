2940557c7a8b7f15ce802d548e591a674efc7daf
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-22 19:04:11 -0300

    v3.3: release para teste de atualizacao automatica

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 24,
-    versionName: "3.2",
+    versionCode: 25,
+    versionName: "3.3",
     url: "https://locarpay-api.vercel.app/download/locarpay.apk"
   });
 }
