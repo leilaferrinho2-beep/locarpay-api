022dedc5237df16a4f59edd8380841c39789d5e4
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-22 15:25:51 -0300

    v3.0: Google Sign-In + multi-tenancy license control

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 21,
-    versionName: "2.9",
+    versionCode: 22,
+    versionName: "3.0",
     url: "https://locarpay-api.vercel.app/download/locarpay.apk"
   });
 }
