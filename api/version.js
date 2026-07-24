6199043767c2acda3d607f09b36c80053863d93a
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-24 18:27:42 -0300

    v3.7: tela novo inquilino igual ao contrato - todos os campos obrigatorios

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 28,
-    versionName: "3.6",
+    versionCode: 29,
+    versionName: "3.7",
     url: "https://locarpay-api.vercel.app/download/locarpay.apk"
   });
 }
