1d6de65cd794782e132bf50cfa96bf825fb089c1
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-25 14:23:13 -0300

    v3.17: fix verificar assinaturas re-busca ownerSigned do Firestore

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 38,
-    versionName: "3.16",
-    url: "https://locarpay-api.vercel.app/download/locarpay-v38.apk"
+    versionCode: 39,
+    versionName: "3.17",
+    url: "https://locarpay-api.vercel.app/download/locarpay-v39.apk"
   });
 }
