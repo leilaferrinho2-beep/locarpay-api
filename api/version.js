03c587268bed7f5f56f70b33e21ecaa61a312035
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-24 21:26:51 -0300

    v3.10: fix loop de atualizacao com SharedPreferences

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 31,
-    versionName: "3.9",
+    versionCode: 32,
+    versionName: "3.10",
     url: "https://locarpay-api.vercel.app/download/locarpay.apk"
   });
 }
