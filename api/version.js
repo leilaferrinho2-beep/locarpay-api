eb065ce7a2345f02c283ce58cbde86b548efaa68
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-24 21:05:04 -0300

    v3.9: bump versao para forcar atualizacao com todos os fixes

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 30,
-    versionName: "3.8",
+    versionCode: 31,
+    versionName: "3.9",
     url: "https://locarpay-api.vercel.app/download/locarpay.apk"
   });
 }
