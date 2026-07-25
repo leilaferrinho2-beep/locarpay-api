820782d7dec31f50cd05db9a8589f0a9241a46d3
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-25 10:25:33 -0300

    v3.12: fix crash ao abrir inquilino com nome null + fix lista inquilinos digitais

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 33,
-    versionName: "3.11",
+    versionCode: 34,
+    versionName: "3.12",
     url: "https://locarpay-api.vercel.app/download/locarpay.apk"
   });
 }
