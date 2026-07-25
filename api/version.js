9713f9f5d84ab6ef666dbaa5362dbd649eed4067
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-25 20:16:02 -0300

    locarpay v3.26 (48): fix charges query por tenantEmail

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 47,
-    versionName: "3.25",
-    url: "https://locarpay-api.vercel.app/download/locarpay-v47.apk"
+    versionCode: 48,
+    versionName: "3.26",
+    url: "https://locarpay-api.vercel.app/download/locarpay-v48.apk"
   });
 }
