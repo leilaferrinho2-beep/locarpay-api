45ff4ed79b8bda752f893ff290d8b552e517df0f
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-25 22:30:13 -0300

    v3.33: fix cpf salvo no cadastro de inquilino

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 54,
-    versionName: "3.32",
-    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.32/locarpay-v54.apk"
+    versionCode: 55,
+    versionName: "3.33",
+    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.33/locarpay-v55.apk"
   });
 }
