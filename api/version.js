1962412ee76f003e1662dcb092300687f6936248
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-25 22:40:52 -0300

    v3.34: verificacao cartao com micro-cobranca + fix cpf no cadastro

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 55,
-    versionName: "3.33",
-    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.33/locarpay-v55.apk"
+    versionCode: 56,
+    versionName: "3.34",
+    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.34/locarpay-v56.apk"
   });
 }
