a339b713e494fa69c51d4787a72b103aa80d97f0
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-26 12:02:26 -0300

    version: 3.40 (versionCode 62)

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 61,
-    versionName: "3.39",
-    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.39/locarpay-v61.apk"
+    versionCode: 62,
+    versionName: "3.40",
+    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.40/locarpay-v62.apk"
   });
 }
