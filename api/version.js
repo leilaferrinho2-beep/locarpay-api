677dc4faf4d22ce6c73f4a1d11bb923883cfbcf8
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-25 22:12:17 -0300

    v3.32: validacao luhn + validade no cartao de credito

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 53,
-    versionName: "3.31",
-    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.31/locarpay-v53.apk"
+    versionCode: 54,
+    versionName: "3.32",
+    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.32/locarpay-v54.apk"
   });
 }
