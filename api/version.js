75d01958abcb4b438e478cc878e6bc7e68c5bae8
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-26 11:40:54 -0300

    v3.39: fix download contrato assinado + clausula pagamento cartao/pix

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 60,
-    versionName: "3.38",
-    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.38/locarpay-v60.apk"
+    versionCode: 61,
+    versionName: "3.39",
+    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.39/locarpay-v61.apk"
   });
 }
