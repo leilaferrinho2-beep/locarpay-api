188eaaf12943e00bc4cb77d41b8600be28e8da9f
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-27 18:26:48 -0300

    version: bump para v3.48 (versionCode 70)

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 69,
-    versionName: "3.47",
-    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.47/locarpay-v69.apk"
+    versionCode: 70,
+    versionName: "3.48",
+    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.48/locarpay-v70.apk"
   });
 }
