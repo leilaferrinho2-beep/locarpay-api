4c7a4fe67e38fcc9ddd049f03f6255d687c68dcf
temperodacasa <sistematemperodacasa@gmail.com>
2026-07-28 17:48:19 -0300

    locarpay v3.60: melhora busca de URL do contrato assinado na Assinafy

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({
-    versionCode: 81,
-    versionName: "3.59",
-    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.59/locarpay-v81.apk"
+    versionCode: 82,
+    versionName: "3.60",
+    url: "https://github.com/leilaferrinho2-beep/locarpay-api/releases/download/v3.60/locarpay-v82.apk"
   });
 }
