92a024979ef3106ef603d3c805b2752ccdb2d8a4
temperodacasa <barudomauriciomdt@gmail.com>
2026-08-29 18:21:00 -0300

    feat: seletor de documento (RG/CNH/CNH Digital) no cadastro de locacao - v333/6.04

--- update-version.cjs
+++ update-version.cjs
@@ -3,8 +3,8 @@ const { readFileSync, writeFileSync } = require('fs');
 const sa = JSON.parse(readFileSync('C:/Users/denis/Downloads/locarpayapp-firebase-adminsdk-fbsvc-e92d24aa50.json', 'utf8'));
 admin.initializeApp({ credential: admin.credential.cert(sa) });
 const db = admin.firestore();
-const versionCode = 332;
-const versionName = '6.03';
+const versionCode = 333;
+const versionName = '6.04';
 const url = 'https://www.ilocarpay.com.br/download/ilocarpay.apk';
 (async () => {
   await db.collection('config').doc('app').set({ versionCode, versionName, url });
