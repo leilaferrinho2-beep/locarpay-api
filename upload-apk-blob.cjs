85bdcf601a393776c053b8575100f89caf16c104
temperodacasa <barudomauriciomdt@gmail.com>
2026-09-06 20:10:34 -0300

    fix: loop update dialog + tema site 1 botao toggle

--- upload-apk-blob.cjs
+++ upload-apk-blob.cjs
@@ -12,8 +12,8 @@ const sa = JSON.parse(readFileSync('C:/Users/denis/Downloads/locarpayapp-firebas
 admin.initializeApp({ credential: admin.credential.cert(sa) });
 const db = admin.firestore();
 
-const versionCode = 349;
-const versionName = '6.20';
+const versionCode = 350;
+const versionName = '6.21';
 const apkBuffer = readFileSync('C:/LocarPay/app/build/outputs/apk/release/app-release.apk');
 
 (async () => {
